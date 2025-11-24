660ddc19a22bc19ba3490662b72433fd5c484a3e
Jaime Martin <141837620+Jaime888888@users.noreply.github.com>
2025-11-24 00:41:09 -0800

    Handle missing userId in favorites servlet

---
+++ build-support/stubs/jakarta/servlet/annotation/WebServlet.java
@@ -0,0 +1,8 @@
+package jakarta.servlet.annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WebServlet {
+    String name() default "";
+    String[] urlPatterns() default {};
+}
